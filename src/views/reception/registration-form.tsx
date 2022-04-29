ac5fd9075f8f45e2d718a822c43f564195816ab7
ruthgdb <ruthgd2000@gmail,com>
2022-04-29 15:41:14 +0300

    text box icons added

--- src/views/reception/registration-form.tsx
+++ src/views/reception/registration-form.tsx
@@ -1,6 +1,21 @@
 import Grid from '@mui/material/Grid'
 import { Card, Typography, CardContent, Button, CardActions } from '@mui/material'
 import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+
+import Phone from 'mdi-material-ui/Phone'
+import EmailOutline from 'mdi-material-ui/EmailOutline'
+import AccountOutline from 'mdi-material-ui/AccountOutline'
+import CalendarMonthIcon from 'mdi-material-ui/CalendarMonth'
+import CityIcon from 'mdi-material-ui/City'
+import HouseIcon from 'mdi-material-ui/Home'
+import StreetIcon from 'mdi-material-ui/RoadVariant'
+
+import SubcityIcon from 'mdi-material-ui/TownHall'
+
+// import LocationCityIcon from 'mdi-material-ui/LocationCity'
+
+// import LocationCityIcon from '@mui/icons-material/LocationCity'
 
 export default function PatientRegistrationForm() {
   return (
@@ -18,19 +33,80 @@ export default function PatientRegistrationForm() {
                 </Typography>
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' fullWidth label='Full Name' placeholder='carterLeonard' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='Full Name'
+                  placeholder='Rediet Demisse'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <AccountOutline />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' fullWidth type='email' label='Email' placeholder='carterleonard@gmail.com' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  type='email'
+                  label='Email'
+                  placeholder='ruthgd2000@gmail.com'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <EmailOutline />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' fullWidth label='Phone Number' placeholder='+251 987654321' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='Phone Number'
+                  placeholder='+251 987654321'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <Phone />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' fullWidth label='Date of Birth' placeholder='01/01/2000' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='Date of Birth'
+                  placeholder='01/01/2000'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <CalendarMonthIcon />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' fullWidth label='Gender' placeholder='Female' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='Gender'
+                  placeholder='Female'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <AccountOutline />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item xs={12}>
                 {/* <Divider sx={{ marginBottom: 0 }} /> */}
@@ -41,36 +117,130 @@ export default function PatientRegistrationForm() {
                 </Typography>
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' fullWidth label='Full Name' placeholder='Leonard' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='Full Name'
+                  placeholder='Rediet Demisse'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <AccountOutline />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' type='email' fullWidth label='Phone' placeholder='+251 987654321' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='Phone'
+                  placeholder='+251 987654321'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <Phone />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
-              <Grid item xs={12}>
-                {/* <Divider sx={{ marginBottom: 0 }} /> */}
-              </Grid>
+              <Grid item xs={12}></Grid>
               <Grid item xs={12}>
                 <Typography variant='body2' sx={{ fontWeight: 600, mb: 3 }}>
                   Address Information
                 </Typography>
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' fullWidth label='City' placeholder='Leonard' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='City'
+                  placeholder='Addis Ababa'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <CityIcon />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' fullWidth label='Woreda' placeholder='Carter' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='Woreda'
+                  placeholder='04'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <HouseIcon />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' fullWidth label='Sub City' placeholder='Leonard' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='Sub City'
+                  placeholder='Bole'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <SubcityIcon />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField size='small' fullWidth label='Kebele' placeholder='Carter' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='Kebele'
+                  placeholder='32'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <CityIcon />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item sx={{ mb: 8 }} xs={12} sm={6}>
-                <TextField size='small' fullWidth label='Street' placeholder='Leonard' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='Street'
+                  placeholder='Mauritania street'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <StreetIcon />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
               <Grid item sx={{ mb: 8 }} xs={12} sm={6}>
-                <TextField size='small' fullWidth label='House Number' placeholder='Carter' />
+                <TextField
+                  size='small'
+                  fullWidth
+                  label='House Number'
+                  placeholder='432'
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position='start'>
+                        <HouseIcon />
+                      </InputAdornment>
+                    )
+                  }}
+                />
               </Grid>
             </Grid>
           </CardContent>
