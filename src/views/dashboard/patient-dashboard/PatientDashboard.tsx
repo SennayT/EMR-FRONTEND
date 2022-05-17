19460b69720f5d90fe131826054eef2b0f94155e
ruthgdb <ruthgd2000@gmail,com>
2022-05-17 08:39:01 +0300

    vitals added

--- src/views/dashboard/patient-dashboard/PatientDashboard.tsx
+++ src/views/dashboard/patient-dashboard/PatientDashboard.tsx
@@ -6,32 +6,85 @@ import PatientDiagnosis from 'src/views/patient-details/PatientDiagnosis'
 import user from 'src/data/userData'
 
 export default function PatientDashboard() {
+  const vitalCategories = [
+    {
+      name: 'Heart Rate',
+      value: '117',
+      measuredBy: '/87',
+      image: '/images/heart.png'
+    },
+    {
+      name: 'Fever',
+      value: '40',
+      measuredBy: 'c',
+      image: '/images/fever.png'
+    },
+    {
+      name: 'Blood Pressure',
+      value: '75',
+      measuredBy: '/123',
+      image: '/images/bp.jpg'
+    },
+    {
+      name: 'Height',
+      value: '170',
+      measuredBy: 'cm',
+      image: '/images/height.jpg'
+    },
+    {
+      name: 'Weight',
+      value: '123',
+      measuredBy: 'c',
+      image: '/images/weight.jpg'
+    },
+    {
+      name: 'BMI',
+      value: '20',
+      measuredBy: '.3',
+      image: '/images/bmi.jpg'
+    }
+  ]
+
   return (
     <Card>
       <Grid container spacing={7}>
         <Grid sx={{ mx: 4, my: 4 }} item xs={5}>
           <PatientDiagnosis user={user} />
         </Grid>
-        <Grid sx={{ mx: 4, my: 4, px: 4, py: 4 }} container xs={6}>
-          <Grid item height={140} xs={5} sx={{ backgroundColor: 'white', borderRadius: 1, mx: 5, my: 4, px: 4 }}>
-            <Grid container spacing={1}>
-              <Typography sx={{ mx: 3, my: 7 }} variant='subtitle1'>
-                Heart Rate
-              </Typography>
-              <Grid item xs={6}>
-                <img src='/images/heart.png' alt='heart rate' height={60} />
+        <Grid item sx={{ mx: 4, my: 4, px: 4, py: 4 }} container xs={6}>
+          {vitalCategories.map(function (vital) {
+            return (
+              <Grid
+                spacing={3}
+                key={vital.name}
+                item
+                height={140}
+                xs={5}
+                sx={{ backgroundColor: 'white', borderRadius: 1, px: 4, mx: 5 }}
+              >
+                <Grid container>
+                  <Grid item xs={12}>
+                    <Typography sx={{ mx: 3, my: 5, display: 'block' }} variant='subtitle1'>
+                      {vital.name}
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <img src={vital.image} alt='heart rate' height={70} />
+                  </Grid>
+                  <Grid item xs={5}>
+                    <Typography sx={{ display: 'inline' }} variant='h4'>
+                      {vital.value}
+                      <Typography sx={{ display: 'inline' }} variant='subtitle1'>
+                        {vital.measuredBy}
+                      </Typography>
+                    </Typography>
+                  </Grid>
+                </Grid>
               </Grid>
-              <Grid item xs={5}>
-                <Typography sx={{ display: 'inline' }} variant='h4'>
-                  117
-                  <Typography sx={{ display: 'inline' }} variant='subtitle1'>
-                    l/ml
-                  </Typography>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item height={140} xs={5} sx={{ backgroundColor: 'white', borderRadius: 2, mx: 5, my: 4 }}>
+            )
+          })}
+          {/* <Grid item height={140} xs={5} sx={{ backgroundColor: 'white', borderRadius: 2, mx: 5, my: 4 }}>
             <Grid container sx={{ mx: 4 }}>
               <Grid item xs={12}>
                 <Typography sx={{ px: 3, pt: 4, pb: 2 }} variant='h5'>
@@ -104,7 +157,7 @@ export default function PatientDashboard() {
                 23.1
               </Grid>
             </Grid>
-          </Grid>
+          </Grid> */}
         </Grid>
       </Grid>
     </Card>
