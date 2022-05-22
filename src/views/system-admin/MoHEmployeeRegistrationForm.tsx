09cb79b442283c6b3364af3cb8d675678280e9df
ruthgdb <ruthgd2000@gmail,com>
2022-05-22 22:21:07 +0300

    bug fix

--- src/views/system-admin/MoHEmployeeRegistrationForm.tsx
+++ src/views/system-admin/MoHEmployeeRegistrationForm.tsx
@@ -40,18 +40,12 @@ export default function ResearcherRegistrationForm() {
         name: name,
         email: email,
         phone: phone,
-<<<<<<< HEAD
-        gender: gender,
-        address: address
-      }
-=======
         gender: 'female',
         age: 32,
         healthCenterId: 4,
         address: address
       },
       registeredBy: user.id
->>>>>>> d44ba0255da364f564a8dfdcd21845f161d54fc2
     }
 
     axios.post(`https://capstone-backend-0957-11-v2.herokuapp.com/moh-employee`, body).then(response => {
