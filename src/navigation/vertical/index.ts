c3c183d376f44411fbd225d686231dff9a0a981c
ruthgdb <ruthgd2000@gmail,com>
2022-06-21 19:46:20 +0300

    moh employee page added

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -30,7 +30,7 @@ const navigation = (session: Session): VerticalNavItemsType => {
     })
   }
 
-  if (session.role === 'Researcher' || session.role === 'MohEmployee') {
+  if (session.role === 'Researcher' || session.role === 'Moh Employee') {
     pagesSection.push({
       sectionTitle: 'pages'
     })
@@ -99,7 +99,6 @@ const navigation = (session: Session): VerticalNavItemsType => {
   }
 
   if (session.role === 'Doctor' || session.role === 'LabExpert') {
-
     pagesSection.push(
       {
         sectionTitle: 'pages'
