deb786ff46cb6237ef34b6efc3a2e7ddce3cc02b
Sennay <wsennay@gmail.com>
2022-06-23 00:48:45 +0300

    fix lab result

--- src/pages/patient/view/results/list.tsx
+++ src/pages/patient/view/results/list.tsx
@@ -1,25 +1,30 @@
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import requests from "src/utils/repository";
-import CardImgTop from "src/views/cards/CardImgTop";
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import requests from 'src/utils/repository'
+import CardImgTop from 'src/views/cards/CardImgTop'
 
 const ViewList = () => {
   const [results, setResults] = useState([])
   const { data: session } = useSession()
-  const router = useRouter();
+  const router = useRouter()
   useEffect(() => {
-    session.role == 'Doctor' ?
-      requests.get(`/investigation-request/${router.query.currId}/lab-results`, session ? session.accessToken.toString() : "").then(response => {
-        setResults(response.data)
-      })
-      : requests.get(`/lab-result/user/filled`, session ? session.accessToken.toString() : "").then(response => {
-        setResults(response.data)
-      })
+    session.role == 'Doctor'
+      ? requests
+          .get(
+            `/investigation-request/${router.query.invId}/lab-results`,
+            session ? session.accessToken.toString() : ''
+          )
+          .then(response => {
+            setResults(response.data)
+          })
+      : requests
+          .get(`/lab-result/${router.query.invId}`, session ? session.accessToken.toString() : '')
+          .then(response => {
+            setResults([response.data])
+          })
   }, [])
 
-  return(results.length ? results.map(res => <CardImgTop result={res} />) : <p>No Result Data Entered</p>)
-
-
+  return results.length != 0 ? results.map(res => <CardImgTop result={res} />) : <p>No Result Data Entered</p>
 }
-export default ViewList;
+export default ViewList
