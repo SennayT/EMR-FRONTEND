2d7f1c59716927d30051e86ae8cc4619dded635d
Sennay <wsennay@gmail.com>
2022-06-23 01:20:48 +0300

    convert to heroku

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,8 +2,8 @@ import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import axios from 'axios'
 
-//const herokuUrl = 'https://capstone-backend-0957-11-v2.herokuapp.com'
-const herokuUrl = 'http://localhost:4000'
+const herokuUrl = 'https://capstone-backend-0957-11-v2.herokuapp.com'
+//const herokuUrl = 'http://localhost:4000'
 const url = `${herokuUrl}/auth/signin`
 
 export default NextAuth({
